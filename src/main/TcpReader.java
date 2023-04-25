c9f33dce7927e5aee90930bcd1a5794807d33f9f
Aneta <aneta.strzyzewska@outlook.com>
2023-04-25 14:55:53 +0200

    quick test implementation to ensure input from tcpdump can be piped on Windows; set up artifact generation

--- src/main/TcpReader.java
+++ src/main/TcpReader.java
@@ -1,9 +1,24 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TcpReader {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        List<String> output = new ArrayList<>();
+
+        for(int i = 0; i < 10; i++) {
+            output.add(reader.readLine());
+        }
+        reader.close();
+
+        output.forEach(System.out::println);
     }
 
 }
